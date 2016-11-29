6e0cc19a77f636f445f0b9bc763421d5224b6ced
Pierre-Alexandre ADAMSKI <pierre-alexandre.adamski@isen-lille.fr>
2016-11-29 20:21:58 +0100

    OK

---
+++ src/CinderProjectApp.cpp
@@ -0,0 +1,42 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+#include "OscMessage.h"
+#include "Osc
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class CinderProjectApp : public App {
+  public:
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
+private:
+    osc::Message    message;
+    osc::
+};
+
+void CinderProjectApp::setup()
+{
+    
+}
+
+void CinderProjectApp::mouseDown( MouseEvent event )
+{
+}
+
+void CinderProjectApp::update()
+{
+    
+}
+
+void CinderProjectApp::draw()
+{
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP( CinderProjectApp, RendererGl )
